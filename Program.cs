a49fcce1fd15bc6688b262c5a995eae6c0f557c8
Rouzok <98026745+Rouzok@users.noreply.github.com>
2026-04-20 23:41:11 +0700

    menambahkan class TeamMembers

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using modul7_Kelompok_4;
 
 DataMahasiswa103082400009 data = new DataMahasiswa103082400009();
+TeamMembers teamMembers = new TeamMembers();
 data.ReadJSON();
+teamMembers.ReadJSON();

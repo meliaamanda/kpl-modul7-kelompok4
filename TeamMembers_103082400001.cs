adda6ac5f57a9a0378a74aaa0d9ba98a670cc8ec
Miranisa3113 <miratunn91@gmail.com>
2026-04-21 01:20:28 +0700

    menambahkan class TeamMember_103082400001

---
+++ TeamMembers_103082400001.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Collections.Generic;
+
+class TeamMembers103082400001
+{
+    public List<Member> members { get; set; }
+
+    public class Member
+    {
+        public string nim { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public int age { get; set; }
+        public string gender { get; set; }
+    }
+
+    public static void ReadJSON()
+    {
+        string json = File.ReadAllText("jurnal7_2_103082400001.json");
+        var data = JsonSerializer.Deserialize<TeamMembers103082400001>(json);
+
+        Console.WriteLine("Team member list:");
+        foreach (var m in data.members)
+        {
+            Console.WriteLine($"{m.nim} {m.firstName} {m.lastName} ({m.age} {m.gender})");
+        }
+    }
+}

48feebfb6ec8cab471ed3159c65d178a5a9ffe27
Rouzok <98026745+Rouzok@users.noreply.github.com>
2026-04-21 00:00:24 +0700

    menambahkan class Glossary

--- Program.cs
+++ Program.cs
@@ -2,5 +2,14 @@
 
 DataMahasiswa103082400009 data = new DataMahasiswa103082400009();
 TeamMembers teamMembers = new TeamMembers();
+GlossaryItem glosarryItem = new GlossaryItem();
+Console.WriteLine("----------Class DataMahasiswa----------");
 data.ReadJSON();
+Console.WriteLine();
+
+Console.WriteLine("----------Class TeamMembers----------");
 teamMembers.ReadJSON();
+Console.WriteLine();
+
+Console.WriteLine("----------Class Glossary----------");
+glosarryItem.ReadJSON();
